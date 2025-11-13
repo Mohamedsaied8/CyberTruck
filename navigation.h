eda65c23e905126cea2810689fd730c96b8d2e16
Mohamed Saied <infint_hero@hotmail.com>
2025-11-13 20:07:39 +0200

    adding MPU

--- navigation.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-class Navigation {
-public:
-    Navigation() = default;
-    void navigateTo(const std::string& destination);
-    void setCurrentLocation(const std::string& location);
-    std::string getCurrentLocation() const;
-};
